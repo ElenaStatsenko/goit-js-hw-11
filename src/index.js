3bd123de7ab7e9823e7177d8686e05cf163b870d
Mykhailo Statsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-06-22 22:49:28 +0300

    начала делать  задачу

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-11
+// import axios from "axios";
+
+// axios.defaults.headers.common["x-api-key"] = "live_fksKLT6b3rWXK0Tu5oRKNInMZzCgNp9DLE15zuaKYxGuWwhtQAtLVAUkFZrwCO5a";
+
+const key = 'live_fksKLT6b3rWXK0Tu5oRKNInMZzCgNp9DLE15zuaKYxGuWwhtQAtLVAUkFZrwCO5a';
+const url = ` https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key= ${key}`
+
+selectForm = document.querySelector('.breed-select');
+
+fetch('https://api.thecatapi.com/v1/images/search?breed_ids=${}').then(data => {
+    console.log(data)
+})
