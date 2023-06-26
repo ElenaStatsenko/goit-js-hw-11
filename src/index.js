100a14d49bc75b8389a5c5bc7e4f27ebf4f5681e
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-06-26 17:52:55 +0300

    добавила разметку

--- src/index.js
+++ src/index.js
@@ -12,56 +12,16 @@ fetch('https://api.thecatapi.com/v1/breeds').then(response=> {
     
  return response.json();
 }).then(cat => {
-    const catBreed = cat.map(({id, name}) => `${id} ${name}`
+    const catBreed = cat.map(({id, name}) => `
+    <select class="breed-select">
+    <option value= ${id}>${name}</option>  
+    </select>`
     ).join('')
     console.log(catBreed);
-renderBreed ({id, name});
+    selectForm.innerHTML = catBreed;
+
 })
 }
-function renderBreed({id, name}) {
-    
 
-}
-
-// onst gallaryContainer = document.querySelector('.gallery'); 
-
-// const images = galleryItems.map(({preview, original, description}) => `
-
-// <li class="gallery__item">
-//    <a class="gallery__link" href='${original}'>
-//       <img class="gallery__image" src='${preview}' alt='${description}' />
-//    </a>
-// </li>`).join(''); 
-
-// function renderSelect() {
-//     // Создание элемента select
-//     var selectElement = document.createElement('select');
-  
-//     // Добавление опций
-//     var option1 = document.createElement('option');
-//     option1.text = 'Опция 1';
-//     selectElement.add(option1);
-  
-//     var option2 = document.createElement('option');
-//     option2.text = 'Опция 2';
-//     selectElement.add(option2);
-  
-//     var option3 = document.createElement('option');
-//     option3.text = 'Опция 3';
-//     selectElement.add(option3);
-  
-//     // Добавление события изменения значения
-//     selectElement.addEventListener('change', function() {
-//       console.log('Выбрано значение:', selectElement.value);
-//     });
-  
-//     // Добавление select на страницу
-//     document.body.appendChild(selectElement);
-//   }
-  
-//   // Вызов функции для рендеринга select
-//   renderSelect();
- 
-// gallaryContainer.insertAdjacentHTML('beforeend', images); 
 
 
