e7c19134acce26ec94e9e2e7e34769b58ac8c30b
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-06-27 19:39:19 +0000

    Deploying to gh-pages from @ ElenaStatsenko/goit-js-hw-11@1b35226ba66a5185973b66c169bb9a551db10688 🚀

--- index.0aa0c106.js
+++
@@ -1,2 +0,0 @@
-selectForm=document.querySelector(".breed-select"),selectForm.addEventListener("DOMContentLoaded",void fetch("https://api.thecatapi.com/v1/breeds").then((e=>e.json())).then((e=>{const t=e.map((({id:e,name:t})=>`\n    \n    <option value= ${e}>${t}</option>  \n    `)).join("");console.log(t),selectForm.innerHTML=t}))),selectionForm.addEventListener("click",(breedId,void fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`).then((e=>e.json())).then((e=>{console.log(e)}))));
-//# sourceMappingURL=index.0aa0c106.js.map
